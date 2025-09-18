b849a0117eaeaddd0c9dfdddcc477376883c9c42
Priyanshi Jain <priyanshijain1265@gmail.com>
2025-09-19 01:49:13 +0530

    added category-wise filters

--- client/src/components/Analytics.js
+++ client/src/components/Analytics.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { Progress } from "antd";
 const Analytics = ({ alltransaction }) => {
+    const categories = [
+    "salary",
+    "tip",
+    "project",
+    "food",
+    "movie",
+    "bills",
+    "medical",
+    "fee",
+    "tax",
+  ];
   // total transaction
   const totalTransaction = alltransaction.length;
   const totalIncomeTransactions = alltransaction.filter(
@@ -92,6 +103,83 @@ const Analytics = ({ alltransaction }) => {
               </div>
             </div>
           </div>
+        </div>
+               <div className="col-md-4">
+          <div className="card">
+            <div className="card-header">Total TurnOver : {totalTurnover}</div>
+            <div className="card-body">
+              <h5 className="text-success">Income : {totalIncomeTurnover}</h5>
+              <h5 className="text-danger">Expense : {totalExpenseTurnover}</h5>
+              <div>
+                <Progress
+                  type="circle"
+                  strokeColor={"green"}
+                  className="mx-2"
+                  percent={totalIncomeTurnoverPercent.toFixed(0)}
+                />
+                <Progress
+                  type="circle"
+                  strokeColor={"red"}
+                  className="mx-2"
+                  percent={totalExpenseTurnoverPercent.toFixed(0)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-md-4">
+          <h4>Categorywise Income</h4>
+          {categories.map((category) => {
+            const amount = alltransaction
+              .filter(
+                (transaction) =>
+                  transaction.type === "income" &&
+                  transaction.category === category
+              )
+              .reduce((acc, transaction) => acc + transaction.amount, 0);
+            return (
+              amount > 0 && (
+                <div className="card">
+                  <div className="card-body">
+                    <h5>{category}</h5>
+                    <Progress
+                      percent={((amount / totalIncomeTurnover) * 100).toFixed(
+                        0
+                      )}
+                    />
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </div>
+        <div className="col-md-4">
+          <h4>Categorywise Expense</h4>
+          {categories.map((category) => {
+            const amount = alltransaction
+              .filter(
+                (transaction) =>
+                  transaction.type === "expense" &&
+                  transaction.category === category
+              )
+              .reduce((acc, transaction) => acc + transaction.amount, 0);
+            return (
+              amount > 0 && (
+                <div className="card">
+                  <div className="card-body">
+                    <h5>{category}</h5>
+                    <Progress
+                      percent={((amount / totalExpenseTurnover) * 100).toFixed(
+                        0
+                      )}
+                    />
+                  </div>
+                </div>
+              )
+            );
+          })}
         </div>
       </div>
     </>
